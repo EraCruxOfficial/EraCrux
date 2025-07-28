3b310447c3fe63d91415fbbf13784e7156a54d29
Divyansh Raj <divyanshraj0408@gmail.com>
2025-07-28 21:43:49 +0530

    feat: nextjs init

---
+++ eracrux/app/page.tsx
@@ -0,0 +1,11 @@
+import Header1 from '@/components/mvpblocks/header-1.tsx';
+import Globe3D from '@/components/mvpblocks/3dglobe.tsx';
+
+export default function App() {
+  return (
+    <div >
+            <Header1 />
+            <Globe3D />
+    </div>
+  );
+}
