935cc37a229eed4a9a0b9bd2644ffb24da0d578c
Divyansh Raj <divyanshraj0408@gmail.com>
2025-07-31 14:52:11 +0530

    fix: marquee delete

--- eracrux/app/page.tsx
+++ eracrux/app/page.tsx
@@ -3,12 +3,6 @@ import Globe3D from '@/components/mvpblocks/3dglobe';
 import FeaturesSectionDemo from '@/components/features-section-demo-3';
 import { Blog8 } from '@/components/blog8';
 import FooterGlow from '@/components/mvpblocks/footer-glow';
-import {
-  Marquee,
-  MarqueeContent,
-  MarqueeFade,
-  MarqueeItem,
-} from '@/components/ui/kibo-ui/marquee';
 
 export default function App() {
   return (
@@ -20,21 +14,6 @@ export default function App() {
       <FeaturesSectionDemo />
       <Blog8 />
       <FooterGlow />
-      {/* <Marquee>
-        <MarqueeFade side="left" />
-        <MarqueeFade side="right" />
-        <MarqueeContent>
-          {new Array(10).fill(null).map((_, index) => (
-            <MarqueeItem className="h-32 w-32" key={index}>
-              <img
-                alt={`Placeholder ${index}`}
-                className="overflow-hidden rounded-full"
-                src={`/excel.png`}
-              />
-            </MarqueeItem>
-          ))}
-        </MarqueeContent>
-      </Marquee> */}
     </div>
   );
 }
