e7a5d81508c3dbf5dcd2112c0c84bc56508f1c12
Divyansh Raj <divyanshraj0408@gmail.com>
2025-09-21 12:39:30 +0530

    homepage

--- eracrux/components/mvpblocks/header-1.tsx
+++ eracrux/components/mvpblocks/header-1.tsx
@@ -15,7 +15,7 @@ interface NavItem {
 }
 
 const navItems: NavItem[] = [
-  { name: 'Home', href: '#home' },
+  { name: 'Home', href: '/' },
   { name: 'Features', href: '#features' },
   {
     name: 'Products',
